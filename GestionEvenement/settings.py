b7c84aacf57528fc3e203bfbad6700659fc78ba5
mehdybenromdhane <mehdybenromdhane9@gmail.com>
2025-09-02 12:37:12 +0100

    configurations

--- GestionEvenement/settings.py
+++ GestionEvenement/settings.py
@@ -31,7 +31,19 @@ SECRET_KEY = 'django-insecure-50r_lx%5@y-sv@s48%n6#q$(0%@f@no5_tl5cm%2a$8y*=*$uz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+
+# Render automatically provides this env var
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
+# CSRF trusted origins (needed for Django 4+)
+CSRF_TRUSTED_ORIGINS = [
+    f"https://{RENDER_EXTERNAL_HOSTNAME}"
+] if RENDER_EXTERNAL_HOSTNAME else []
 
 
 # Application definition
