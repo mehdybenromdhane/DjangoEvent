879e5564307805f6eed353b0180ad693f760c324
mehdybenromdhane <mehdybenromdhane9@gmail.com>
2025-09-02 12:31:26 +0100

    configurations

--- GestionEvenement/settings.py
+++ GestionEvenement/settings.py
@@ -29,7 +29,7 @@ load_dotenv(dotenv_path)
 SECRET_KEY = 'django-insecure-50r_lx%5@y-sv@s48%n6#q$(0%@f@no5_tl5cm%2a$8y*=*$uz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -131,11 +131,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"   # <-- Add this line
 
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = BASE_DIR / "staticfiles"   # <-- Add this line
-
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
       # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
