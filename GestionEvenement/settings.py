7c241c4bd600b02df610d9bdaf06b7617b92524d
mehdybenromdhane <mehdybenromdhane9@gmail.com>
2025-09-03 13:09:04 +0100

    configurations

--- GestionEvenement/settings.py
+++ GestionEvenement/settings.py
@@ -102,7 +102,7 @@ DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
         default='postgresql://postgres:postgres@localhost:5432/DjangoEvent',
-        conn_max_age=600
+        conn_max_age=600,ssl_require=True,
     )
     
 }
